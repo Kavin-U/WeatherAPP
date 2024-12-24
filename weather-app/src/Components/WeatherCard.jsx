0e6cd6889743ad219eabdcb91273383b9a19f22a
Kavin-U <ukavin7@gmail.com>
2024-12-24 19:46:43 +0530

    Fixing Responsive bugs

--- weather-app/src/Components/WeatherCard.jsx
+++ weather-app/src/Components/WeatherCard.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import WeatherImgs from "./WeatherImgs";
 import clearsky from "../assets/weather/Clear_sky.jpg";
 import clear from "../assets/weather_icons/clear-sky.png";
+import Sunrise from "../assets/weather_icons/sunrise.png";
+import Sunset from "../assets/weather_icons/sunset-.png";
 import weathericn from "./WeatherIcs";
 
 const WeatherCard = ({ weatherData }) => {
@@ -22,51 +24,69 @@ const WeatherCard = ({ weatherData }) => {
     <div
       style={style}
       id="weather-main"
-      className="w-[90%] h-[90%] p-2 flex flex-col justify-between mt-3 rounded-lg bg-white bg-opacity-30 sm:bg-opacity-60"
+      className="w-[90%] h-[90%] p-2 flex flex-col gap-4 justify-between mt-3 rounded-lg bg-white bg-opacity-30 sm:bg-opacity-60"
     >
-      <div className="w-full h-[250px] rounded-md bg-white bg-opacity-35 p-5 sm:p-7 flex justify-between">
+      <div className="w-full h-[250px] rounded-md bg-white bg-opacity-35 p-5 sm:p-7 sm:flex block justify-between">
         {data ? (
           <>
             <div>
-              <h3 className="text-2xl">
+              <h3 className="text-xl sm:text-2xl">
                 {data.city_name}, {data.country_code}
               </h3>
-              <h1 className="text-6xl mt-3">{data.temp}°C</h1>
+              <h1 className="sm:text-6xl text-4xl mt-3">{data.temp}°C</h1>
               <p className="text-xl mt-3 capitalize">
                 {data.weather.description}
               </p>
             </div>
-            <div className="hidden sm:hidden md:flex justify-evenly w-[600px] h-fit p-4 text-black rounded-lg bg-gray-500 bg-opacity-35">
+            <div className="hidden sm:hidden lg:flex justify-evenly w-[600px] h-fit p-4 text-black rounded-lg bg-gray-500 bg-opacity-35">
               <div className="flex flex-col gap-3">
                 <p>
-                  Lattitude <br /> {data.lat}
+                  <span className="font-semibold">Lattitude</span> <br />{" "}
+                  {data.lat}
                 </p>
                 <p>
-                  Longitude <br /> {data.lon}
+                  <span className="font-semibold">Longitude</span> <br />{" "}
+                  {data.lon}
                 </p>
               </div>
-              <div>
-                <p>
-                  Sunrise <br /> {data.sunrise}{" "}
-                </p>
-              </div>
-              <div>
-                <p>
-                  Sunset <br /> {data.sunset}{" "}
-                </p>
-              </div>
-              <div className="flex flex-col gap-3">
-                <p>
-                  TimeZone <br /> {data.timezone}{" "}
-                </p>
-                <p>Wind Direction <br/> {data.wind_cdir_full} </p>
-              </div>
-            </div>
-            <div>
-              <div className="text-sm">{data.ob_time}</div>
               <div>
                 <img
-                  className="w-[70px] h-[70px] float-right mt-4 sm:w-[100px] sm:h-[100px]"
+                  className="w-[50px] h-[50px] mb-3"
+                  src={Sunrise}
+                  alt="Sunrise"
+                />
+                <p>
+                  <span className="font-semibold">Sunrise</span> <br />{" "}
+                  {data.sunrise}{" "}
+                </p>
+              </div>
+              <div>
+                <img
+                  className="w-[50px] h-[50px] mb-3"
+                  src={Sunset}
+                  alt="Sunset"
+                />
+                <p>
+                  <span className="font-semibold">Sunset</span> <br />{" "}
+                  {data.sunset}{" "}
+                </p>
+              </div>
+              <div className="flex flex-col gap-3">
+                <p>
+                  <span className="font-semibold">TimeZone</span> <br />{" "}
+                  {data.timezone}{" "}
+                </p>
+                <p>
+                  <span className="font-semibold">Wind Direction</span> <br />{" "}
+                  {data.wind_cdir_full}
+                </p>
+              </div>
+            </div>
+            <div className="min-w-[130px] relative">
+              <div className="text-sm absolute bottom-3 right-2 sm:static sm:float-right">{data.ob_time}</div>
+              <div>
+                <img
+                  className="w-[70px] h-[70px] float-none sm:float-right mt-4 sm:w-[100px] sm:h-[100px]"
                   src={
                     weathericn[data.weather.description.toLowerCase()] || clear
                   }
@@ -78,6 +98,42 @@ const WeatherCard = ({ weatherData }) => {
           <p className="text-xl">Loading weather data...</p>
         )}
       </div>
+      {data ? (
+      <div className="block overflow-auto sm:overflow-hidden sm:flex lg:hidden sm:justify-evenly w-[100%] gap-10  mx-auto h-fit p-4 text-black rounded-lg bg-gray-500 bg-opacity-35 scrollbar-thin">
+        <div className="block mb-2 sm:flex sm:mb-0 flex-col gap-3">
+          <p>
+            <span className="font-semibold">Lattitude</span> <br className="hidden sm:block" /> {data.lat}
+          </p>
+          <p>
+            <span className="font-semibold">Longitude</span> <br className="hidden sm:block" /> {data.lon}
+          </p>
+        </div>
+        <div className="flex gap-4 justify-evenly">
+        <div>
+          <img className="w-[50px] h-[50px] mb-3" src={Sunrise} alt="Sunrise" />
+          <p>
+            <span className="font-semibold">Sunrise</span> <br /> {data.sunrise}{" "}
+          </p>
+        </div>
+        <div>
+          <img className="w-[50px] h-[50px] mb-3" src={Sunset} alt="Sunset" />
+          <p>
+            <span className="font-semibold">Sunset</span> <br /> {data.sunset}{" "}
+          </p>
+        </div>
+        </div>
+        <div className="block sm:flex flex-col gap-3">
+          <p>
+            <span className="font-semibold">TimeZone</span> <br className="hidden sm:block" />{" "}
+            {data.timezone}{" "}
+          </p>
+          <p>
+            <span className="font-semibold">Wind Direction</span> <br className="hidden sm:block" />{" "}
+            {data.wind_cdir_full}
+          </p>
+        </div>
+      </div>
+      ):(<div></div>)}
       <div className="w-full flex justify-evenly rounded-md items-center bg-opacity-35 h-[100px] bg-white">
         {data ? (
           <>
